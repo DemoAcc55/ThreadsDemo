7a3d06e9a2e62d8185f22b4d525f6304a2572cc5
Danil Filippov <damarreve@list.ru>
2021-04-02 22:56:05 +0400

    Листинг 4.27 (не доделан)

--- main.cpp
+++ main.cpp
@@ -1717,7 +1717,48 @@ void foo425()
 /* Листинг 4.26 (стр ) */
 /* Конец листинга 4.26 */
 
-/* Листинг 4.27 (стр ) */
+/* Листинг 4.27 (стр 160) */
+
+/*
+void process_data(data_source& source, data_sink& sink)
+{
+    unsigned const concurrency = thread::hardware_concurrency();
+    unsigned const num_threads = (concurrency > 0) ? concurrency : 2;
+
+    vector<data_chunk> chunks;
+
+    auto split_source = [&]
+    {
+        if (!source.done())
+        {
+            data_block current_block = source.get_next_data_block();
+            chunks = divide_into_chunks(current_block, num_threads);
+        }
+    };
+
+    split_source();
+
+    result_block result;
+
+    experimental::flex_barrier sync(num_threads, [&] {
+        sink.write_data(move(result));
+        split_source();
+        return -1;
+    });
+    vector<joining_thread> threads(num_threads);
+
+    for (unsigned i = 0; i < num_threads; i++)
+    {
+        threads[i] = joining_thread([&, i] {
+            while (!source.done())
+            {
+                result.set_chunk(i, num_threads, process(chunks[i]));
+                sync.arrive_and_wait();
+            }
+        });
+    }
+}
+*/
 /* Конец листинга 4.27 */
 
 int main()
